7b7de1b768bba7871f0a93cac30883c9cfa38c8c
CHRIS TROTTER <root@localhost.localdomain>
2014-12-14 20:36:52 +0000

    flask-hello-world

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
+app.config["DEBUG"] = True
+
 @app.route('/')
 def home():
 	return "Hello world"
@@ -9,6 +11,10 @@ def home():
 @app.route('/welcome')	
 def welcome():
 	return render_template("welcome.html")
+
+@app.route('/test/<search_query>')
+def search(search_query):
+	return search_query
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -22,7 +28,29 @@ def login():
 
 	return render_template('login.html', error=error)
 
+@app.route("/integer/<int:value>")
+def int_type(value):
+	print value + 1
+	return "correct"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+	print value + 1
+	return "correct"
+
+@app.route("/path/<path:value>")
+def path_type(value):
+	print value
+	return "correct"
+
+@app.route('/name/<name>')
+def index(name):
+	if name.lower() == "michael":
+		return "Hello, {}".format(name), 200
+	else:
+		return "Not found", 404
+
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run()
 
